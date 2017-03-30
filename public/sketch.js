fc163dc5603daea9baa7b0a64c92c106e78288a7
Matt Kenney <mk365@duke.edu>
2017-03-30 08:58:46 -0400

    first commit

---
+++ public/sketch.js
@@ -0,0 +1,40 @@
+
+var socket;
+
+function setup(){
+   createCanvas(800, 800);
+
+   socket = io.connect('http://localhost:3000');
+   socket.on('mouse', newDrawing);
+
+}
+
+function newDrawing(data){
+
+  fill(random(0, 255));
+  rect(data.x, data.y, 80, 80);
+
+}
+
+function mouseDragged(){
+	console.log(mouseX + ' ' + mouseY);
+
+	var data = {
+		x: mouseX, 
+		y: mouseY
+	}
+
+	socket.emit('mouse', data);
+
+
+  if (mouseIsPressed) {
+    fill(random(0, 255), random(0, 255), random(0, 255));
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
+
+function draw(){ 
+
+}
---
+++ public/sound.js
@@ -0,0 +1,16 @@
+
+// Tracks the mouse's horizonal movement across the window.
+environment.start();
+var synth = flock.synth({
+    synthDef: {
+        ugen: "flock.ugen.sinOsc",
+        freq: {
+            ugen: "flock.ugen.mouse.cursor",
+            rate: "control",
+            mul: 880,
+            add: 110
+        },
+        mul: 0.25
+    }
+});
+     
