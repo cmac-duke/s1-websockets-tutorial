ab82f1a91b521e91545f5d414f2360fef64e8fa9
Matt Kenney <mk365@duke.edu>
2017-03-30 09:20:27 -0400

    idk

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var socket = require('socket.io');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 var app = express();
 var server = app.listen(port);
 
